a713b716ab5de01b4230a1c7377f209e08624ee1
Kevs <kev.sunlee@gmail.com>
2021-08-15 17:25:46 -0700

    vite boilerplate

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="changeName(1)"></button>
+    <div>{{ name }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+
+  setup() {
+    const name = ref('Kevin')
+
+    const changeName = (newName: string) => {
+      name.value = newName
+    }
+
+    return {
+      changeName,
+      name,
+    }
+  },
+})
+</script>
+
+<style></style>
